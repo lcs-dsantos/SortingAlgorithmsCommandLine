907249e183f06974371e21628cb13040e0d2eec8
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2018-03-07 12:05:05 -0500

    Removed the lowest value and instead at the sorted list

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -24,29 +24,56 @@ print("Press any key to generate the unsorted list.", terminator: "")
 let input = readLine()
 
 // Make an empty array / list
-var numbers : [Int] = []
+var unsortedList : [Int] = []
 
 // Populate the array
-for _ in 1...10 {
-    numbers.append( random(between: 1, and: 50) )
-}
+//for _ in 1...10 {
+//    numbers.append( random(between: 1, and: 50) )
+//}
+unsortedList.append(5)
+unsortedList.append(6)
+unsortedList.append(3)
+unsortedList.append(4)
+unsortedList.append(7)
 
 // Show the user the contents of the array
 print("-----")
 print("The contents of the unsorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...unsortedList.count - 1 {
+    print("Index \(i), value: \(unsortedList[i])")
 }
 print("-----")
 
 // ----------- Write code to sort the array here -----------
 print("Now sorting the array...")
 
+// Create new array (sortedList)
+var sortedList : [Int] = []
+
+
+// assume the first card in my list is the lowest
+var lowestNumberValue = unsortedList[0]
+var lowestNumberPosition = 0
+
+//Check positions 1...4 to see if there is any card that is lower
+for currentPosition in 1...4 {
+    // The porpouse of this if statement is to see if there is any other lowest card
+    if unsortedList[currentPosition] < lowestNumberValue {
+        //If the other card is lowest then my first lowest it becomes my new lowest.
+        lowestNumberValue = unsortedList[currentPosition]
+        lowestNumberPosition = currentPosition
+    }
+}
+
+// Get the lowest number and transfer it for the soreted list.
+unsortedList.remove(at: lowestNumberPosition)
+sortedList.append( lowestNumberValue )
+
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
 print("The contents of the sorted list:")
-for i in 0...numbers.count - 1 {
-    print("Index \(i), value: \(numbers[i])")
+for i in 0...unsortedList.count - 1 {
+    print("Index \(i), value: \(unsortedList[i])")
 }
 print("-----")
 
