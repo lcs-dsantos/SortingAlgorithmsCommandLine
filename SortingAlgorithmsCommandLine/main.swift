aa3a3ec1bbff26c6e1468ef32f2f88c1feb53bfd
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2018-03-08 12:06:32 -0500

    created the sorted list with success.

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -50,24 +50,31 @@ print("Now sorting the array...")
 // Create new array (sortedList)
 var sortedList : [Int] = []
 
-
-// assume the first card in my list is the lowest
-var lowestNumberValue = unsortedList[0]
-var lowestNumberPosition = 0
-
-//Check positions 1...4 to see if there is any card that is lower
-for currentPosition in 1...4 {
-    // The porpouse of this if statement is to see if there is any other lowest card
-    if unsortedList[currentPosition] < lowestNumberValue {
-        //If the other card is lowest then my first lowest it becomes my new lowest.
-        lowestNumberValue = unsortedList[currentPosition]
-        lowestNumberPosition = currentPosition
+// Sorting the entire list
+for underscore in 1...4{
+    // assume the first card in my list is the lowest
+    var lowestNumberValue = unsortedList[0]
+    var lowestNumberPosition = 0
+    
+    //Check positions 1...4 to see if there is any card that is lower
+    for currentPosition in 1...unsortedList.count - 1 {
+        // The porpouse of this if statement is to see if there is any other lowest card
+        if unsortedList[currentPosition] < lowestNumberValue {
+            //If the other card is lowest then my first lowest it becomes my new lowest.
+            lowestNumberValue = unsortedList[currentPosition]
+            lowestNumberPosition = currentPosition
+        }
     }
+    
+    // Get the lowest number and transfer it for the soreted list.
+    unsortedList.remove(at: lowestNumberPosition)
+    sortedList.append( lowestNumberValue )
+    
 }
 
-// Get the lowest number and transfer it for the soreted list.
-unsortedList.remove(at: lowestNumberPosition)
-sortedList.append( lowestNumberValue )
+// Removed to the unsorted list and add to the sorted list
+sortedList.append(unsortedList[0])
+unsortedList.remove(at: 0)
 
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
